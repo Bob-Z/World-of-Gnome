521b1d588da7b7c04da1bba656d9593f5137230f
Bob-Z <carabobz@gmail.com>
2013-11-08 14:45:41 +0100

    Smarter way to find suitable return tile from dungeon

--- data/demo01/script/add_dungeon.lua
+++ data/demo01/script/add_dungeon.lua
@@ -7,16 +7,40 @@ if x == -1 or y == -1 then
 end
 
 map = character_get_map(id);
+-- test to know where to move back the player
+cx=x-1;
+cy=y;
+tile_type = map_get_tile_type(map,cx,cy);
+if tile_type ~= "ground" then
+	cx=x+1;
+	cy=y;
+	tile_type = map_get_tile_type(map,cx,cy);
+	if tile_type ~= "ground" then
+		cx=x;
+		cy=y-1;
+		tile_type = map_get_tile_type(map,cx,cy);
+		if tile_type ~= "ground" then
+			cx=x;
+			cy=y+1;
+			tile_type = map_get_tile_type(map,cx,cy);
+			if tile_type ~= "ground" then
+				print_text_id(id, "No space left for going back from this dungeon")
+				return
+			end
+		end
+	end
+end
 
 new_map = map_new(32,32,64,64,"dungeon_wall");
 tile = "dungeon_up";
 map_set_tile(new_map,tile,16,15);
 tile = "dungeon_ground";
 map_set_tile(new_map,tile,16,16);
+	
 event = map_add_event(new_map,"goto.lua",16,15)
 map_add_event_param(new_map,event,map);
-map_add_event_param(new_map,event,x-1);
-map_add_event_param(new_map,event,y);
+map_add_event_param(new_map,event,cx);
+map_add_event_param(new_map,event,cy);
 
 tile = "dungeon_down";
 map_set_tile(map,tile,x,y);
