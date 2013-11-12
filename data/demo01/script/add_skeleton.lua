4f2c5dfa89f0c2a280970be446eb6102c9cc9dcb
Bob-Z <carabobz@gmail.com>
2013-11-12 14:53:50 +0100

    [demo01] Allow skeleton to be alive !

--- data/demo01/script/add_skeleton.lua
+++ data/demo01/script/add_skeleton.lua
@@ -1,1 +1,15 @@
+player = player_get_id();
 id = character_create_from_template("skeleton");
+--need instantiation early to allow others modifications
+character_set_npc(id,1);
+
+x = character_get_selected_map_tile_x(player);
+y = character_get_selected_map_tile_y(player);
+if x == -1 or y == -1 then
+        print_text_id(id, "You must choose a tile")
+        return
+end
+map = character_get_map(player);
+
+character_set_pos(id,map,x,y);
+
