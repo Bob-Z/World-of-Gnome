d2d422c9d1f2fbe423bd1e81202f37d704384dc1
Bob-Z <carabobz@gmail.com>
2013-11-12 15:12:40 +0100

    [demo01] no diagonal move for AI

--- data/demo01/script/ai_random.lua
+++ data/demo01/script/ai_random.lua
@@ -4,8 +4,14 @@ map = character_get_map(id);
 pos_x = character_get_x(id);
 pos_y = character_get_y(id);
 
-new_pos_x = pos_x + math.random(-1,1);
-new_pos_y = pos_y + math.random(-1,1);
+new_pos_x = pos_x;
+new_pos_y = pos_y;
+rand = math.random(-1,1)
+if math.random(0,1) == 0 then
+	new_pos_x = pos_x + rand
+else
+	new_pos_y = pos_y + rand;
+end
 
 character_set_pos(id,map,new_pos_x,new_pos_y);
 
