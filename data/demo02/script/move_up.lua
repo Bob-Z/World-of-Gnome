9ba2028693340503fddf859d2189d46392d2389a
Bob-Z <Bob-Z@users.noreply.github.com>
2014-01-24 15:04:52 +0100

    LUA doesn't need trailing ";"

--- data/demo02/script/move_up.lua
+++ data/demo02/script/move_up.lua
@@ -1,17 +1,17 @@
-id = player_get_id();
+id = player_get_id()
 
-map = character_get_map(id);
-pos_x = character_get_x(id);
-pos_y = character_get_y(id);
-map_x = character_get_map_x(id);
-map_y = character_get_map_y(id);
+map = character_get_map(id)
+pos_x = character_get_x(id)
+pos_y = character_get_y(id)
+map_x = character_get_map_x(id)
+map_y = character_get_map_y(id)
 
-new_pos_x = pos_x;
-new_pos_y = pos_y - 1 ;
+new_pos_x = pos_x
+new_pos_y = pos_y - 1
 
 if new_pos_y < 0 then
         return
 end
 
-character_set_pos(id,map,new_pos_x,new_pos_y);
+character_set_pos(id,map,new_pos_x,new_pos_y)
 
