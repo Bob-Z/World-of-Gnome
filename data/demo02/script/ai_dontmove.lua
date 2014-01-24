daf1dade0fa5b652dc61fda95df96db9376ad366
Bob-Z <Bob-Z@users.noreply.github.com>
2014-01-24 14:59:16 +0100

    LUA doesn't need trailing ";"

--- data/demo02/script/ai_dontmove.lua
+++ data/demo02/script/ai_dontmove.lua
@@ -1,11 +1,10 @@
-id = player_get_id();
+id = player_get_id()
 
-map = character_get_map(id);
-pos_x = character_get_x(id);
-pos_y = character_get_y(id);
+map = character_get_map(id)
+pos_x = character_get_x(id)
+pos_y = character_get_y(id)
 
-character_set_pos(id,map,pos_x,pos_y);
+character_set_pos(id,map,pos_x,pos_y)
 
 -- return the time in ms before the next NPC AI action
-return 1000000;
-
+return 1000000
