6c1ffc2a69d397d290b7ffda775d4ba63e7d5da5
Bob-Z <Bob-Z@users.noreply.github.com>
2014-01-24 11:39:45 +0100

    LUA doesn't need trailing ";"

--- data/demo01/script/action/add_black_dragon.lua
+++ data/demo01/script/action/add_black_dragon.lua
@@ -1,14 +1,14 @@
-player = player_get_id();
-id = character_create_from_template("black_dragon");
+player = player_get_id()
+id = character_create_from_template("black_dragon")
 --need instantiation early to allow others modifications
-character_set_npc(id,1);
+character_set_npc(id,1)
 
-x = character_get_selected_map_tile_x(player);
-y = character_get_selected_map_tile_y(player);
+x = character_get_selected_map_tile_x(player)
+y = character_get_selected_map_tile_y(player)
 if x == -1 or y == -1 then
         print_text_id(id, "You must choose a tile")
         return
 end
-map = character_get_map(player);
+map = character_get_map(player)
 
-character_set_pos(id,map,x,y);
+character_set_pos(id,map,x,y)
