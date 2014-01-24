4738fb1d75646aa26dc4e8060afdf1e6ce13542d
Bob-Z <Bob-Z@users.noreply.github.com>
2014-01-24 11:28:35 +0100

    LUA doesn't need trailing ";"

--- data/demo00/script/ai_random.lua
+++ data/demo00/script/ai_random.lua
@@ -1,14 +1,14 @@
-id = player_get_id();
+id = player_get_id()
 
-map = character_get_map(id);
-pos_x = character_get_x(id);
-pos_y = character_get_y(id);
+map = character_get_map(id)
+pos_x = character_get_x(id)
+pos_y = character_get_y(id)
 
-new_pos_x = pos_x + math.random(-1,1);
-new_pos_y = pos_y + math.random(-1,1);
+new_pos_x = pos_x + math.random(-1,1)
+new_pos_y = pos_y + math.random(-1,1)
 
-character_set_pos(id,map,new_pos_x,new_pos_y);
+character_set_pos(id,map,new_pos_x,new_pos_y)
 
 -- return the time in ms before the next NPC AI action
-return 1000;
+return 1000
 
