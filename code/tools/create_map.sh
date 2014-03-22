4a7144d8c24adfd0a8dfd044e899f023266f16b3
Bob-Z <carabobz@gmail.com>
2014-03-22 21:43:58 +0100

    [tools] create tiles and images files in subdirs

--- code/tools/create_map.sh
+++ code/tools/create_map.sh
@@ -12,12 +12,12 @@ IMAGE_SIZE_Y=`identify -format "%[fx:h]" $1`
 TILE_X=$((IMAGE_SIZE_X / TILE_SIZE_X))
 EXACT=$((IMAGE_SIZE_X % TILE_SIZE_X))
 if [ EXACT != 0 ];then
-	TILE_X=$((TILE_X+1))
+        TILE_X=$((TILE_X+1))
 fi
 TILE_Y=$((IMAGE_SIZE_Y / TILE_SIZE_Y))
 EXACT=$((IMAGE_SIZE_Y % TILE_SIZE_Y))
 if [ EXACT != 0 ];then
-	TILE_Y=$((TILE_Y+1))
+        TILE_Y=$((TILE_Y+1))
 fi
 
 FILE_NAME=`basename $1`
@@ -25,10 +25,10 @@ MAP_NAME=`echo $FILE_NAME | awk -F. '{ print $1 }'`
 EXT_NAME=`echo $FILE_NAME | awk -F. '{ print $2 }'`
 
 mkdir $OUTPUT_DIR/map
-mkdir $OUTPUT_DIR/tile
-mkdir $OUTPUT_DIR/image
+mkdir $OUTPUT_DIR/tile/$MAP_NAME
+mkdir $OUTPUT_DIR/image/$MAP_NAME
 
-convert $1 -crop $TILE_SIZE -background $FILL_COLOR -extent $TILE_SIZE $OUTPUT_DIR/image/${MAP_NAME}_%05d.$EXT_NAME
+convert $1 -crop $TILE_SIZE -background $FILL_COLOR -extent $TILE_SIZE $OUTPUT_DIR/image/${MAP_NAME}/%05d.$EXT_NAME
 
 OUTPUT_MAP=$OUTPUT_DIR/map/$MAP_NAME
 
@@ -38,13 +38,13 @@ echo "tile_size_x = $TILE_SIZE_X;" >> $OUTPUT_MAP
 echo "tile_size_y = $TILE_SIZE_Y;" >> $OUTPUT_MAP
 echo -n "set = [ " >> $OUTPUT_MAP
 set=""
-for i in `ls  $OUTPUT_DIR/image/${MAP_NAME}_*$EXT_NAME`
+for i in `ls  $OUTPUT_DIR/image/${MAP_NAME}/*$EXT_NAME`
 do
-	filename=`basename $i`
-	tilename=`echo $filename | awk -F. '{ print $1 }'`
-	echo "image = \"$filename\";" > $OUTPUT_DIR/tile/$tilename
-#	echo -n "\"$tilename\"," >> $OUTPUT_MAP
-	set=`echo -n "$set\"$tilename\","`
+        filename=`basename $i`
+        tilename=`echo $filename | awk -F. '{ print $1 }'`
+        echo "image = \"$filename\";" > $OUTPUT_DIR/tile/$MAP_NAME/$tilename
+#       echo -n "\"$tilename\"," >> $OUTPUT_MAP
+        set=`echo -n "$set\"$MAP_NAME/$tilename\","`
 
 done
 #remove trailing coma
