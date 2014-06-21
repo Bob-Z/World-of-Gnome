a78f7be3d182614e327dd1228709e8e9bd24ef2b
Bob-Z <carabobz@gmail.com>
2014-06-21 20:45:02 +0200

    [tools] fix map creation from single source image

--- code/tools/create_map.sh
+++ code/tools/create_map.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-OUTPUT_DIR="/tmp"
-TILE_SIZE_X=64
-TILE_SIZE_Y=64
+OUTPUT_DIR="/tmp/wog"
+TILE_SIZE_X=16
+TILE_SIZE_Y=16
 TILE_SIZE="${TILE_SIZE_X}x${TILE_SIZE_Y}"
 FILL_COLOR="black"
 
@@ -11,12 +11,12 @@ IMAGE_SIZE_Y=`identify -format "%[fx:h]" $1`
 
 TILE_X=$((IMAGE_SIZE_X / TILE_SIZE_X))
 EXACT=$((IMAGE_SIZE_X % TILE_SIZE_X))
-if [ EXACT != 0 ];then
+if [ ! $EXACT == 0 ];then
         TILE_X=$((TILE_X+1))
 fi
 TILE_Y=$((IMAGE_SIZE_Y / TILE_SIZE_Y))
 EXACT=$((IMAGE_SIZE_Y % TILE_SIZE_Y))
-if [ EXACT != 0 ];then
+if [ ! $EXACT == 0 ];then
         TILE_Y=$((TILE_Y+1))
 fi
 
@@ -24,11 +24,12 @@ FILE_NAME=`basename $1`
 MAP_NAME=`echo $FILE_NAME | awk -F. '{ print $1 }'`
 EXT_NAME=`echo $FILE_NAME | awk -F. '{ print $2 }'`
 
-mkdir $OUTPUT_DIR/map
-mkdir $OUTPUT_DIR/tile/$MAP_NAME
-mkdir $OUTPUT_DIR/image/$MAP_NAME
+mkdir -p $OUTPUT_DIR/map
+mkdir -p $OUTPUT_DIR/tile/$MAP_NAME
+mkdir -p $OUTPUT_DIR/image/$MAP_NAME
 
-convert $1 -crop $TILE_SIZE -background $FILL_COLOR -extent $TILE_SIZE $OUTPUT_DIR/image/${MAP_NAME}/%05d.$EXT_NAME
+#convert $1 -crop $TILE_SIZE -background $FILL_COLOR -extent $TILE_SIZE $OUTPUT_DIR/image/${MAP_NAME}/%05d.$EXT_NAME
+convert $1 -crop ${TILE_SIZE} +repage  +adjoin $OUTPUT_DIR/image/${MAP_NAME}/%05d.$EXT_NAME
 
 OUTPUT_MAP=$OUTPUT_DIR/map/$MAP_NAME
 
@@ -42,7 +43,7 @@ for i in `ls  $OUTPUT_DIR/image/${MAP_NAME}/*$EXT_NAME`
 do
         filename=`basename $i`
         tilename=`echo $filename | awk -F. '{ print $1 }'`
-        echo "image = \"$filename\";" > $OUTPUT_DIR/tile/$MAP_NAME/$tilename
+        echo "image = \"$MAP_NAME/$filename\";" > $OUTPUT_DIR/tile/$MAP_NAME/$tilename
 #       echo -n "\"$tilename\"," >> $OUTPUT_MAP
         set=`echo -n "$set\"$MAP_NAME/$tilename\","`
 
