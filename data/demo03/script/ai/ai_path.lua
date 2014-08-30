8865a5731944caae1ab9162f53703f9f4f6474f1
Bob-Z <carabobz@gmail.com>
2014-08-30 22:32:26 +0200

    [demo03] new demo

---
+++ data/demo03/script/ai/ai_path.lua
@@ -0,0 +1,29 @@
+function f ()
+
+x = { 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9 }
+y = { 18,19,20,21,22,23,24,24,24,24,24,23,22,21,20,19,18,18,18,18 }
+num_move = 20
+
+id = player_get_id()
+
+index = character_attribute_get(id,"path_index")
+
+if index == -1 then
+	index = 1
+end
+
+index = index+1
+if index > num_move then
+	index = 1
+end
+
+map = character_get_map(id)
+
+character_set_pos(id,map,x[index],y[index])
+
+character_attribute_set(id,"path_index",index)
+
+-- return the time in ms before the next NPC AI action
+return 1000
+
+end
---
+++ data/demo03/script/move_right.lua
@@ -0,0 +1,19 @@
+function f ()
+
+id = player_get_id()
+
+map = character_get_map(id)
+pos_x = character_get_x(id)
+pos_y = character_get_y(id)
+map_w = character_get_map_w(id)
+map_h = character_get_map_h(id)
+
+new_pos_x = pos_x + 1
+new_pos_y = pos_y
+
+if new_pos_x >= map_w then
+        return
+end
+
+character_set_pos(id,map,new_pos_x,new_pos_y)
+end
