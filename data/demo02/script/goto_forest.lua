d6fa68678ed5ea44673840d91ca0a1ed021bb8d9
Bob-Z <carabobz@gmail.com>
2014-08-30 21:32:34 +0200

    [demo02] rename test to village and test2 to forest

--- data/demo02/script/goto_forest.lua
+++ data/demo02/script/goto_forest.lua
@@ -2,6 +2,6 @@ function f ()
 
 id = player_get_id()
 
-character_set_pos(id,"test2",1,1)
+character_set_pos(id,"forest",1,1)
 
 end
--- data/demo02/script/goto_village.lua
+++ data/demo02/script/goto_village.lua
@@ -2,6 +2,6 @@ function f ()
 
 id = player_get_id()
 
-character_set_pos(id,"test",1,1)
+character_set_pos(id,"village",1,1)
 
 end
