dd0ac0f8a347b82bfb2116fdbe03fb25322b353b
Bob-Z <carabobz@gmail.com>
2014-09-04 11:14:44 +0200

    [demo02] Fix map_get_tile call parameter

--- data/demo02/script/get_apple.lua
+++ data/demo02/script/get_apple.lua
@@ -4,7 +4,7 @@ id = player_get_id()
 map = character_get_map(id)
 x = character_get_x(id)
 y = character_get_y(id)
-tile = map_get_tile(map,x,y)
+tile = map_get_tile(map,x,y,0)
 
 if tile == "0" then
 	new_item_id = item_create_from_template("apple")
