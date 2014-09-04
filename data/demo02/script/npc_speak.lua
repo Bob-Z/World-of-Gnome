1d886edafc49713a81a9699d401bde8603908657
Bob-Z <carabobz@gmail.com>
2014-09-04 22:11:54 +0200

    [demo02] send name rather than id when speaking

--- data/demo02/script/npc_speak.lua
+++ data/demo02/script/npc_speak.lua
@@ -1,18 +1,20 @@
 function f (npc,player,keyword)
 
+name = character_get_name(npc);
+
 if keyword == nil then
 	keyword = "start"
 end
 
 if keyword == "start" then
-	speak_send(npc,player,"Hello","icon/greendot.gif","How are you ?","how","icon/greendot.gif","Goodbye","bye")
+	speak_send(name,player,"Hello","icon/greendot.gif","How are you ?","how","icon/greendot.gif","Goodbye","bye")
 end
 
 if keyword == "how" then
-	speak_send(npc,player,"Fine thank you","icon/greendot.gif","Goodbye","bye")
+	speak_send(name,player,"Fine thank you","icon/greendot.gif","Goodbye","bye")
 end
 
 if keyword == "bye" then
-	speak_send(npc,player,"See you !")
+	speak_send(name,player,"See you !")
 end
 end
