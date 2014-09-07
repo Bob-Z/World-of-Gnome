48674684887917c65d647b4d9ee636c685b869e0
Bob-Z <carabobz@gmail.com>
2014-09-07 21:53:46 +0200

    [demo00] Update with regards latest aggro latest changes.

--- data/demo00/script/say_hello.lua
+++ data/demo00/script/say_hello.lua
@@ -1,13 +1,13 @@
-function f (aggro_id)
+function f (target_id, aggro_on)
 
 id = player_get_id()
 
-if aggro_id == "" then
+if aggro_on == "0" then
 	character_attribute_set(id,"already_aggro",0)
 else
 	if character_attribute_get(id,"already_aggro") == 0 then
-		text = string.format("Hello %s, I am %s",character_get_name(aggro_id),character_get_name(id))
-		print_text_id( aggro_id , text)
+		text = string.format("Hello %s, I am %s",character_get_name(target_id),character_get_name(id))
+		print_text_id( target_id , text)
 		character_attribute_set(id,"already_aggro",1)
 	end
 end
