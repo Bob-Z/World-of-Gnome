e399f100a2bd2b1e72c3db01fdb6d0d32df7b418
Bob-Z <carabobz@gmail.com>
2014-09-12 21:40:57 +0200

    [demo03] Add magic lake

--- data/demo03/script/goto.lua
+++ data/demo03/script/goto.lua
@@ -2,6 +2,12 @@ function f (map,x, y)
 
 id = player_get_id()
 
+item = inventory_get_by_name(id,"artefact")
+
+if map == "lake" and item ~= nil then
+	map = "magic_lake"
+end
+
 character_set_pos(id,map,x,y)
 
 end
