33f5cdc4ec9d1018f4762c16ba8bad65753961aa
Bob-Z <carabobz@gmail.com>
2014-09-19 21:29:31 +0200

    [demo00] Fix get_apple.lua script

--- data/demo00/script/get_apple.lua
+++ data/demo00/script/get_apple.lua
@@ -4,9 +4,9 @@ id = player_get_id()
 map = character_get_map(id)
 x = character_get_x(id)
 y = character_get_y(id)
-tile = map_get_tile(map,x,y)
+tile = map_get_tile(map,x,y,0)
 
-if tile == "0" then
+if tile == "tile/black.jpg" then
 	new_item_id = item_create_from_template("apple")
 	inventory_add(id,new_item_id)
 	return
