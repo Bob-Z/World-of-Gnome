e36733725b66e2f5bd618243f9e54942a955c8c0
Bob-Z <carabobz@gmail.com>
2014-09-21 21:37:05 +0200

    [demo00] Ask the user to select a character

--- data/demo00/script/remove_life.lua
+++ data/demo00/script/remove_life.lua
@@ -3,6 +3,11 @@ function f ()
 id = player_get_id()
 
 target = character_get_selected_character_id(id)
+if target == nil then
+        print_text_id(id, "Select a target character")
+        return
+end
+
 character_attribute_change(target,"life",-1)
 
 end
