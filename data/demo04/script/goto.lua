e5133049557a0de9551e01112fcd5512fb5f5a60
Bob-Z <carabobz@gmail.com>
2014-10-11 21:23:02 +0200

    [demo04] Add first house with 2 levels

---
+++ data/demo04/script/goto.lua
@@ -0,0 +1,13 @@
+function f (map,x, y)
+
+id = player_get_id()
+
+item = inventory_get_by_name(id,"artefact")
+
+if map == "lake" and item ~= nil then
+	map = "magic_lake"
+end
+
+character_set_pos(id,map,x,y)
+
+end
