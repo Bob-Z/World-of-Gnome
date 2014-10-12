5bd1a9cf17bb6f667eb6c4dbddca61b93af8742c
Bob-Z <carabobz@gmail.com>
2014-10-12 21:34:41 +0200

    [demo04] Add set_type_* action

---
+++ data/demo04/script/set_type_water.lua
@@ -0,0 +1,13 @@
+function f ()
+
+id = player_get_id()
+x = character_get_selected_map_tile_x(id)
+y = character_get_selected_map_tile_y(id)
+if x == -1 or y == -1 then
+	print_text_id(id, "You must choose a tile")
+	return
+end
+map = character_get_map(id)
+map_set_tile_type(map,"w",x,y)
+
+end
