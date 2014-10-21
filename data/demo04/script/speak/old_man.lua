61619988bf79227941cfbfad789bb8aab64e7257
Bob-Z <carabobz@gmail.com>
2014-10-21 21:28:44 +0200

    [demo04] Changing old man portrait with its mood

--- data/demo04/script/speak/old_man.lua
+++ data/demo04/script/speak/old_man.lua
@@ -24,12 +24,14 @@ if keyword == "sword" then
 		item = item_create_from_template("sword")
 		inventory_add(player,item)
 		text = string.format("You got a new sword. What do you need ?",player_name)
+		portrait = "portrait/old_man_happy.gif"
 	else
-		text = string.format("You don't have enought gold. What do you need ?",player_name)
+		text = string.format("You don't have enought gold, get out !",player_name)
+		portrait = "portrait/old_man_angry.gif"
 	end
-	speak_send(npc,"portrait/old_man.gif",player,text,
+	speak_send(npc,portrait,player,text,
 		"item/sword.gif","Buy a Sword (200 gold)","sword",
-		"item/shield.gif","Buy a Shield","shield (150 gold)",
+		"item/shield.gif","Buy a Shield (150 gold)","shield",
 		"item/bag.gif","Buy a Bag (25 gold)","bag"
 	)
 end
@@ -43,12 +45,14 @@ if keyword == "shield" then
 		item = item_create_from_template("shield")
 		inventory_add(player,item)
 		text = string.format("You got a new shield. What do you need ?",player_name)
+		portrait = "portrait/old_man_happy.gif"
 	else
-		text = string.format("You don't have enought gold. What do you need ?",player_name)
+		text = string.format("You don't have enought gold, get out !",player_name)
+		portrait = "portrait/old_man_angry.gif"
 	end
-	speak_send(npc,"portrait/old_man.gif",player,text,
+	speak_send(npc,portrait,player,text,
 		"item/sword.gif","Buy a Sword (200 gold)","sword",
-		"item/shield.gif","Buy a Shield","shield (150 gold)",
+		"item/shield.gif","Buy a Shield (150 gold)","shield",
 		"item/bag.gif","Buy a Bag (25 gold)","bag"
 	)
 end
@@ -62,12 +66,14 @@ if keyword == "bag" then
 		item = item_create_from_template("bag")
 		inventory_add(player,item)
 		text = string.format("You got a new bag. What do you need ?",player_name)
+		portrait = "portrait/old_man_happy.gif"
 	else
-		text = string.format("You don't have enought gold. What do you need ?",player_name)
+		text = string.format("You don't have enought gold, get out !",player_name)
+		portrait = "portrait/old_man_angry.gif"
 	end
-	speak_send(npc,"portrait/old_man.gif",player,text,
+	speak_send(npc,portrait,player,text,
 		"item/sword.gif","Buy a Sword (200 gold)","sword",
-		"item/shield.gif","Buy a Shield","shield (150 gold)",
+		"item/shield.gif","Buy a Shield (150 gold)","shield",
 		"item/bag.gif","Buy a Bag (25 gold)","bag"
 	)
 end
