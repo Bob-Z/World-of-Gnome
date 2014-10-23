65dcb446657c4f73436cc1a635bb1b6036ca734e
Bob-Z <carabobz@gmail.com>
2014-10-23 23:38:06 +0200

    [demo05] Allow ant to dig

--- data/demo05/script/move.lua
+++ data/demo05/script/move.lua
@@ -1,12 +1,12 @@
 function f (move_x,move_y)
 
-id = player_get_id()
+player_id = player_get_id()
 
-map = character_get_map(id)
-pos_x = character_get_x(id)
-pos_y = character_get_y(id)
-map_w = character_get_map_w(id)
-map_h = character_get_map_h(id)
+map = character_get_map(player_id)
+pos_x = character_get_x(player_id)
+pos_y = character_get_y(player_id)
+map_w = character_get_map_w(player_id)
+map_h = character_get_map_h(player_id)
 
 new_pos_x = pos_x + move_x
 new_pos_y = pos_y + move_y
@@ -24,6 +24,13 @@ if new_pos_y >= map_h then
         return
 end
 
-character_set_pos(id,map,new_pos_x,new_pos_y)
+character_set_pos(player_id,map,new_pos_x,new_pos_y)
+
+tile_type = map_get_tile_type(map,new_pos_x,new_pos_y)
+
+if tile_type == "b" then
+	map_set_tile(map,"d.gif",new_pos_x,new_pos_y,0)
+	map_set_tile_type(map,"d",new_pos_x,new_pos_y)
+end
 
 end
