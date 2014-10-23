94dfa14c744c8dd8e89496992253ce67da11767a
Bob-Z <carabobz@gmail.com>
2014-10-23 11:01:57 +0200

    [demo04] Smarter old man speak script

--- data/demo04/script/speak/old_man.lua
+++ data/demo04/script/speak/old_man.lua
@@ -1,81 +1,50 @@
+price_list = { sword = 200, shield = 150, bag = 25 }
+
+function trade (player,item)
+	gold = inventory_get_by_name(player,"gold")
+	gold_qty = resource_get_quantity(gold)
+	price = price_list[item]
+	if gold_qty >= price then
+		resource_set_quantity(gold,gold_qty-price)
+		new_item = item_create_from_template(item)
+		inventory_add(player,new_item)
+		text = string.format("You got a new %s. What do you need ?",item)
+		portrait = "portrait/old_man_happy.gif"
+	else
+		text = string.format("You don't have enough gold, get out !")
+		portrait = "portrait/old_man_angry.gif"
+	end
+
+	return text,portrait
+end
+
+function talk (npc,portrait,player,text)
+	func_param = {}
+	for k, v in pairs(price_list) do
+		keyword = k
+		price = v
+		speech = string.format("item/%s.gif",keyword)
+		table.insert(func_param, speech)
+		speech = string.format("Buy a %s (%d gold)",keyword,price)
+		table.insert(func_param, speech)
+		table.insert(func_param, keyword)
+	end
+
+	speak_send(npc,portrait,player,text, unpack(func_param))
+end
+
 function f (npc,player,keyword)
 
-if keyword == nil then
-	keyword = "start"
-end
-
-player_name = character_get_name(player)
-
-if keyword == "start" then
-	text = string.format("Hello %s. What do you need ?",player_name)
-	speak_send(npc,"portrait/old_man.gif",player,text,
-		"item/sword.gif","Buy a Sword (200 gold)","sword",
-		"item/shield.gif","Buy a Shield","shield (150 gold)",
-		"item/bag.gif","Buy a Bag (25 gold)","bag"
-	)
-end
-
-if keyword == "sword" then
-	price = 200
-	gold = inventory_get_by_name(player,"gold")
-	gold_qty = resource_get_quantity(gold)
-	if gold_qty >= price then
-		resource_set_quantity(gold,gold_qty-price)
-		item = item_create_from_template("sword")
-		inventory_add(player,item)
-		text = string.format("You got a new sword. What do you need ?",player_name)
-		portrait = "portrait/old_man_happy.gif"
-	else
-		text = string.format("You don't have enought gold, get out !",player_name)
-		portrait = "portrait/old_man_angry.gif"
+	if keyword == nil then
+		keyword = "start"
 	end
-	speak_send(npc,portrait,player,text,
-		"item/sword.gif","Buy a Sword (200 gold)","sword",
-		"item/shield.gif","Buy a Shield (150 gold)","shield",
-		"item/bag.gif","Buy a Bag (25 gold)","bag"
-	)
-end
 
-if keyword == "shield" then
-	price = 150
-	gold = inventory_get_by_name(player,"gold")
-	gold_qty = resource_get_quantity(gold)
-	if gold_qty >= price then
-		resource_set_quantity(gold,gold_qty-price)
-		item = item_create_from_template("shield")
-		inventory_add(player,item)
-		text = string.format("You got a new shield. What do you need ?",player_name)
-		portrait = "portrait/old_man_happy.gif"
+	if keyword == "start" then
+		text = string.format("Hello %s. What do you need ?",character_get_name(player))
+		portrait = "portrait/old_man.gif"
 	else
-		text = string.format("You don't have enought gold, get out !",player_name)
-		portrait = "portrait/old_man_angry.gif"
+		text,portrait = trade(player,keyword)
 	end
-	speak_send(npc,portrait,player,text,
-		"item/sword.gif","Buy a Sword (200 gold)","sword",
-		"item/shield.gif","Buy a Shield (150 gold)","shield",
-		"item/bag.gif","Buy a Bag (25 gold)","bag"
-	)
-end
 
-if keyword == "bag" then
-	prise = 25
-	gold = inventory_get_by_name(player,"gold")
-	gold_qty = resource_get_quantity(gold)
-	if gold_qty >= price then
-		resource_set_quantity(gold,gold_qty-price)
-		item = item_create_from_template("bag")
-		inventory_add(player,item)
-		text = string.format("You got a new bag. What do you need ?",player_name)
-		portrait = "portrait/old_man_happy.gif"
-	else
-		text = string.format("You don't have enought gold, get out !",player_name)
-		portrait = "portrait/old_man_angry.gif"
-	end
-	speak_send(npc,portrait,player,text,
-		"item/sword.gif","Buy a Sword (200 gold)","sword",
-		"item/shield.gif","Buy a Shield (150 gold)","shield",
-		"item/bag.gif","Buy a Bag (25 gold)","bag"
-	)
-end
-
+	talk(npc,portrait,player,text)
 end
