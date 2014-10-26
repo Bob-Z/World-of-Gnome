67d5659f0514673b784f4daefdeb0af157e3a73c
Bob-Z <carabobz@gmail.com>
2014-10-26 21:01:41 +0100

    [demo03] Speech can be closed by speak item

--- data/demo03/script/speak/oracle.lua
+++ data/demo03/script/speak/oracle.lua
@@ -9,17 +9,25 @@ end
 
 if keyword == "start" then
 	text = string.format("Hello %s, I am %s. Would you like to help me ?",player_name,oracle_name)
-	speak_send(npc,"portrait/oracle.png",player,text,"","Yes, of course","yes","","No, thanks","bye")
+	speak_send(npc,"portrait/oracle.png",player,text,
+			"","Yes, of course","yes",
+			"","No, thanks","bye")
 end
 
 if keyword == "yes" then
 	item = item_create_from_template("artefact")
 	map_add_item("cliff",item,5,5);
 	text = string.format("Thanks %s, please go fetch the holy artefact !",player_name)
-	speak_send(npc,"portrait/oracle.png",player,text)
+	speak_send(npc,"portrait/oracle.png",player,text,
+			"","Let's go !","speak_end")
 end
 
 if keyword == "bye" then
-	speak_send(npc,"portrait/oracle.png",player,"See you !")
+	speak_send(npc,"portrait/oracle.png",player,"See you !",
+			"","Bye","speak_end")
+end
+
+if keyword == "speak_end" then
+	speak_send("","",player,"")
 end
 end
