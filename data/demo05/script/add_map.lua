45a54058e41f75ff5f1c4a88515df416974f5a36
Bob-Z <carabobz@gmail.com>
2014-10-27 08:17:56 +0100

    [demo05] More generic add_map script

--- data/demo05/script/add_map.lua
+++ data/demo05/script/add_map.lua
@@ -1,9 +1,13 @@
 function f (map_name,map_x,map_y)
 
-new_map = map_new(map_name,32,32,32,32,"b.gif","b")
+player_id = player_get_id()
+map_w = character_get_map_w(player_id)
+map_h = character_get_map_h(player_id)
+
+new_map = map_new(map_name,map_w,map_h,32,32,"b.gif","b")
 
 if map_y == "0" then
-	for i=0,31 do
+	for i=0,map_w-1 do
 		map_set_tile(map_name,"s.gif",i,0,0)
 		map_set_tile_type(map_name,"s",i,0)
 
