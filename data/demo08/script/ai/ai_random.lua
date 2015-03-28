08b790029c02b021f4e3b47f202cd8260c7f4ef2
Bob-Z <carabobz@gmail.com>
2015-03-28 23:05:12 +0100

    [demo08] update to new layer system

--- data/demo08/script/ai/ai_random.lua
+++ data/demo08/script/ai/ai_random.lua
@@ -15,7 +15,7 @@ else
 	new_pos_y = pos_y + rand
 end
 
-character_set_pos(id,map,new_pos_x,new_pos_y)
+character_set_pos(id,map,0,new_pos_x,new_pos_y)
 
 -- return the time in ms before the next NPC AI action
 return 1000
