538deb6d9e6dbcdd32a170c8bd47c599d36be887
Bob-Z <carabobz@gmail.com>
2015-04-06 21:36:16 +0200

    [demo08] Gryphons can be summoned

---
+++ data/demo08/script/action/summon_gryphon.lua
@@ -0,0 +1,22 @@
+function f ()
+
+player = player_get_id()
+
+x = character_get_selected_map_tile_x(player)
+y = character_get_selected_map_tile_y(player)
+if x == -1 or y == -1 then
+        print_text_id(player, "You must choose a tile")
+        return
+end
+map = character_get_map(player)
+
+id = character_create_from_template("gryphon",map,0,x,y)
+if id == nil then
+        print_text_id(player, "Cannot create gryphon here")
+        return
+end
+
+character_set_npc(id,1)
+
+end
+
