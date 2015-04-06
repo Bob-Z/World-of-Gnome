98bb4d5015389d11f98798a07ca6d38e3904c93b
Bob-Z <carabobz@gmail.com>
2015-04-06 21:42:07 +0200

    [demo08] Only Shyde can create map

--- data/demo08/script/offscreen.lua
+++ data/demo08/script/offscreen.lua
@@ -40,6 +40,11 @@ map_name = string.format("M%d_%d",new_map_x,new_map_y)
 res = character_set_pos(player_id,map_name,0,player_x,player_y)
 
 if res == -1 then
+	-- Only shyde can create map
+	player_type = character_get_type(player_id)
+	if player_type ~= "shyde" then
+		return -1
+	end
 	call_script("add_map.lua",map_name,new_map_x,new_map_y);
         res = character_set_pos(player_id,map_name,0,player_x,player_y)
 
