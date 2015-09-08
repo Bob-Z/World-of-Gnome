b3132e75b2240846788d29313e09bc2f63c28f2f
Bob-Z <carabobz@gmail.com>
2015-09-08 20:36:38 +0200

    [demo09_bis] update

--- data/demo09_bis/script/add_map.lua
+++ data/demo09_bis/script/add_map.lua
@@ -4,12 +4,12 @@ player_id = player_get_id()
 map_w = character_get_map_w(player_id)
 map_h = character_get_map_h(player_id)
 
-new_map = map_new(map_name,0,map_w,map_h,72,72,"tile/gr1.png","fl")
+new_map = map_new(map_name,0,map_w,map_h,32,21,"tile/grass1.png","grass")
 
 map_set_offscreen(map_name,0,"offscreen.lua")
 
-map_set_custom_column(map_name,0,0,54,-36)
-map_set_custom_column(map_name,0,1,54,36)
+--map_set_custom_column(map_name,0,0,54,-36)
+--map_set_custom_column(map_name,0,1,54,36)
 
 map_attribute_set(map_name,"x",map_x)
 map_attribute_set(map_name,"y",map_y)
