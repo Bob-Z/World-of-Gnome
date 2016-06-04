81e2042014db1568e9dbe370d7229c55776f54c3
Bob-Z <carabobz@gmail.com>
2016-06-04 18:19:33 +0200

    [tool] Order tiles a bit

--- data/demo09/tool/create_map.sh
+++ data/demo09/tool/create_map.sh
@@ -8,25 +8,33 @@ for NUM in `ls -d ? | sort`;do
 
 	FILE_QTY=`ls -1 $NUM/WC_*_marquee.zip | wc -l`
 	echo $FILE_QTY files in map $NUM
+	let "WIDTH=($FILE_QTY+1)/2"
 
 	FILE=`ls $NUM/WC_*_marquee.zip`
-	for f in $FILE;do
-		echo "\"character/$f\"," >> $MAP_NAME
-	done
 
 	echo "attribute = {" > $MAP_NAME
         echo "  x = {"	>> $MAP_NAME
         echo "        current = $NUM"	>> $MAP_NAME
 	echo "      }"	>> $MAP_NAME
 	echo "}"	>> $MAP_NAME
-	echo "width = 10"	>> $MAP_NAME
-	echo "height = 10"	>> $MAP_NAME
+	echo "width = $WIDTH"	>> $MAP_NAME
+	echo "height = 3"	>> $MAP_NAME
 	echo "tile_width = 64"	>> $MAP_NAME
 	echo "tile_height = 64"	>> $MAP_NAME
 	echo "warp_x = 0"	>> $MAP_NAME
 	echo "warp_y = 0"	>> $MAP_NAME
 	echo "layer0 = {"	>> $MAP_NAME
 	echo "        set = ["	>> $MAP_NAME
+	CURRENT_TILE=0
+	for f in $FILE;do
+		if [ "$CURRENT_TILE" = "$WIDTH" ];then
+			for i in `seq 1 $WIDTH`;do
+				echo "\"\"," >> $MAP_NAME
+			done
+		fi
+		echo "\"character/$f\"," >> $MAP_NAME
+		let "CURRENT_TILE=$CURRENT_TILE+1"
+	done
 	#last coma
 	echo "\"\"" >> $MAP_NAME
 
