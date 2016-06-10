62e8de5ae28c8f77476613e0e07dbfd9ba4096d2
Bob-Z <carabobz@gmail.com>
2016-06-10 23:19:43 +0200

    [demo09][tool] Generate square map

--- data/demo09/tool/create_map.sh
+++ data/demo09/tool/create_map.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAP_QTY=`ls -d -1 ? | wc -l`
+MAP_QTY=`ls -1 -d [0-9]* | wc -l`
 echo Generating $MAP_QTY maps
 
 for NUM in `ls -d [0-9]* | sort -h`;do
@@ -8,12 +8,18 @@ for NUM in `ls -d [0-9]* | sort -h`;do
 
 	FILE_QTY=`ls -1 $NUM/WC_*_marquee.zip | wc -l`
 	echo $FILE_QTY files in map $NUM
-	let "COLUMN=($FILE_QTY+1)/2"
-	let "ADD_TILE=$FILE_QTY%2"
-	#Add 2 column for map exits and 2 for cancel tiles
-	let "WIDTH=$COLUMN+2+2"
-	FIRST_TILE=2
-	let "LAST_TILE=$COLUMN+2-1"
+
+	LINE=`echo "scale = 0; sqrt($FILE_QTY)" | bc`
+	COLUMN=`echo "scale = 0; $FILE_QTY/$LINE" | bc`
+	let "TOTAL=$LINE*$COLUMN"
+	if [ "$TOTAL" -lt "$FILE_QTY" ];then
+		let "LINE=$LINE+1"
+	fi
+
+	let "COLUMN=$COLUMN*2"
+
+	let "MAP_COLUMN=$COLUMN+1"
+	let "MAP_LINE=$LINE+4"
 
 	FILE=`ls $NUM/WC_*_marquee.zip`
 
@@ -22,71 +28,103 @@ for NUM in `ls -d [0-9]* | sort -h`;do
         echo "        current = $NUM"	>> $MAP_NAME
 	echo "      }"	>> $MAP_NAME
 	echo "}"	>> $MAP_NAME
-	echo "width = $WIDTH"	>> $MAP_NAME
-	echo "height = 3"	>> $MAP_NAME
+	echo "width = $MAP_COLUMN"	>> $MAP_NAME
+	echo "height = $MAP_LINE"	>> $MAP_NAME
 	echo "tile_width = 64"	>> $MAP_NAME
 	echo "tile_height = 64"	>> $MAP_NAME
 	echo "warp_x = 0"	>> $MAP_NAME
 	echo "warp_y = 0"	>> $MAP_NAME
 	echo "layer0 = {"	>> $MAP_NAME
 	echo "        set = ["	>> $MAP_NAME
+
+	#First 2 lines
+	echo "\"tile/fog.png\"," >> $MAP_NAME #upper left cancel tile
+	for i in `seq 2 $COLUMN`;do
+		echo "\"\"," >> $MAP_NAME
+	done
+	echo "\"tile/fog.png\"," >> $MAP_NAME #upper right cancel tile
+	for i in `seq 0 $COLUMN`;do
+		echo "\"\"," >> $MAP_NAME
+	done
+
 	CURRENT_TILE=0
-	echo "\"\"," >> $MAP_NAME #left exit column
-	echo "\"tile/fog.png\"," >> $MAP_NAME #left cancel tile
+	CUR_X=1
+	CUR_Y=1
+	let "HALF_LINE=($LINE/2)+1"
 	for f in $FILE;do
+		if [ "$CUR_Y" = "$HALF_LINE" ];then
+			if [ "$CUR_X" = 1 ];then
+				echo "\"tile/exit.gif\"," >> $MAP_NAME
+			else
+				echo "\"\"," >> $MAP_NAME
+			fi
+		else
+			echo "\"\"," >> $MAP_NAME
+		fi
 		echo "\"character/$f\"," >> $MAP_NAME
 		let "CURRENT_TILE=$CURRENT_TILE+1"
-		if [ "$CURRENT_TILE" = "$COLUMN" ];then
-			echo "\"tile/fog.png\"," >> $MAP_NAME #right cancel tile
-			echo "\"\"," >> $MAP_NAME #right exit column
-			echo "\"tile/exit.gif\"," >> $MAP_NAME #left exit tile
-			echo "\"\"," >> $MAP_NAME #left cancel column
-			for i in `seq $FIRST_TILE $LAST_TILE`;do
+		let "CUR_X=$CUR_X+2"
+		if [ "$CUR_X" -ge $COLUMN ];then
+			if [ "$CUR_Y" = "$HALF_LINE" ];then
+				echo "\"tile/exit.gif\"," >> $MAP_NAME
+			else
 				echo "\"\"," >> $MAP_NAME
-			done
-			echo "\"\"," >> $MAP_NAME #right cancel column
-			echo "\"tile/exit.gif\"," >> $MAP_NAME #exit tile
-			echo "\"\"," >> $MAP_NAME #left exit column
-			echo "\"tile/fog.png\"," >> $MAP_NAME #left cancel tile
+			fi
+			let "CUR_Y=$CUR_Y+1"
+			let "CUR_X=1"
 		fi
 	done
-	if [ "$ADD_TILE" = 1 ];then
-		echo "\"\"," >> $MAP_NAME
-	fi
-	echo "\"tile/fog.png\"," >> $MAP_NAME #right cancel tile
-	echo "\"\"" >> $MAP_NAME #right exit column
 
+	let "ADD_TILE=$MAP_COLUMN-$CUR_X+1"
+	for i in `seq 1 $ADD_TILE`;do
+		echo "\"\"," >> $MAP_NAME
+	done
+
+	#last 2 lines
+	for i in `seq 0 $COLUMN`;do
+		echo "\"\"," >> $MAP_NAME
+	done
+	echo "\"tile/fog.png\"," >> $MAP_NAME #lower left cancel tile
+	for i in `seq 2 $COLUMN`;do
+		echo "\"\"," >> $MAP_NAME
+	done
+	echo "\"tile/fog.png\"" >> $MAP_NAME #lower right cancel tile
+
+	#end of set
 	echo "        ]"	>> $MAP_NAME
 	echo "}"	>> $MAP_NAME
 
+	######################
 	#Events
+	######################
 	echo "event_list: {"	>> $MAP_NAME
 
-	XPOS=2
-	YPOS=0
+	CUR_X=1
+	CUR_Y=2
 	CURRENT_EVENT=0
 	for f in $FILE;do
 		SHORT_NAME=`echo $f | sed 's/.*WC_//g' | sed 's/_marquee.zip//g'`
-		if [ "$CURRENT_EVENT" = "$COLUMN" ];then
-			XPOS=2
-			YPOS=2
-		fi
 		echo "E$CURRENT_EVENT = {" >> $MAP_NAME
-		echo "   pos_x = $XPOS" >> $MAP_NAME
-		echo "   pos_y = $YPOS" >> $MAP_NAME
+		echo "   pos_x = $CUR_X" >> $MAP_NAME
+		echo "   pos_y = $CUR_Y" >> $MAP_NAME
 		echo "   script = \"set_sprite.lua\"" >> $MAP_NAME
 		echo "   param = ( \"$NUM\", \"$SHORT_NAME\" )" >> $MAP_NAME
 		echo "}" >> $MAP_NAME
 
+		let "CUR_X=$CUR_X+2"
+		if [ "$CUR_X" -gt "$COLUMN" ];then
+			CUR_X=1
+			let "CUR_Y=$CUR_Y+1"
+		fi
 		let "CURRENT_EVENT=$CURRENT_EVENT+1"
-		let "XPOS=$XPOS+1"
 	done
 
 	#Left exit event
 	let "PREV_NUM=$NUM-1"
+	let "HALF_LINE=$HALF_LINE+1"
 	echo "E$CURRENT_EVENT = {" >> $MAP_NAME
 	echo "   pos_x = 0" >> $MAP_NAME
-	echo "   pos_y = 1" >> $MAP_NAME
+	echo "   pos_y = $HALF_LINE" >> $MAP_NAME
 	echo "   script = \"goto.lua\"" >> $MAP_NAME
 	echo "   param = ( \"C$PREV_NUM\", \"1\", \"1\" )" >> $MAP_NAME
 	echo "}" >> $MAP_NAME
@@ -94,24 +132,25 @@ for NUM in `ls -d [0-9]* | sort -h`;do
 	let "CURRENT_EVENT=$CURRENT_EVENT+1"
 
 	#Right exit event
-	let "RIGHT_X=$WIDTH-1"
-	let "NEXT_NUM=$NUM+1"
+	let "LAST_COLUMN=$MAP_COLUMN-1"
+	let "NEXT_MAP=$NUM+1"
 	echo "E$CURRENT_EVENT = {" >> $MAP_NAME
-	echo "   pos_x = $RIGHT_X" >> $MAP_NAME
-	echo "   pos_y = 1" >> $MAP_NAME
+	echo "   pos_x = $LAST_COLUMN" >> $MAP_NAME
+	echo "   pos_y = $HALF_LINE" >> $MAP_NAME
 	echo "   script = \"goto.lua\"" >> $MAP_NAME
-	if [ "$NEXT_NUM" -gt "$MAP_QTY" ];then
+	if [ "$NEXT_MAP" -ge "$MAP_QTY" ];then
 		echo "   param = ( \"M0_0\", \"10\", \"10\" )" >> $MAP_NAME
 	else
-		echo "   param = ( \"C$NEXT_NUM\", \"1\", \"1\" )" >> $MAP_NAME
+		echo "   param = ( \"C$NEXT_MAP\", \"1\", \"1\" )" >> $MAP_NAME
 	fi
 	echo "}" >> $MAP_NAME
 
 	let "CURRENT_EVENT=$CURRENT_EVENT+1"
 
 	#Left cancel events
+	let "LAST_LINE=MAP_LINE-1"
 	echo "E$CURRENT_EVENT = {" >> $MAP_NAME
-	echo "   pos_x = 1" >> $MAP_NAME
+	echo "   pos_x = 0" >> $MAP_NAME
 	echo "   pos_y = 0" >> $MAP_NAME
 	echo "   script = \"set_sprite.lua\"" >> $MAP_NAME
 	echo "   param = ( \"$NUM\", \"\" )" >> $MAP_NAME
@@ -120,8 +159,8 @@ for NUM in `ls -d [0-9]* | sort -h`;do
 	let "CURRENT_EVENT=$CURRENT_EVENT+1"
 
 	echo "E$CURRENT_EVENT = {" >> $MAP_NAME
-	echo "   pos_x = 1" >> $MAP_NAME
-	echo "   pos_y = 2" >> $MAP_NAME
+	echo "   pos_x = 0" >> $MAP_NAME
+	echo "   pos_y = $LAST_LINE" >> $MAP_NAME
 	echo "   script = \"set_sprite.lua\"" >> $MAP_NAME
 	echo "   param = ( \"$NUM\", \"\" )" >> $MAP_NAME
 	echo "}" >> $MAP_NAME
@@ -129,9 +168,8 @@ for NUM in `ls -d [0-9]* | sort -h`;do
 	let "CURRENT_EVENT=$CURRENT_EVENT+1"
 
 	#Right cancel events
-	let "RIGHT_X=$WIDTH-2"
 	echo "E$CURRENT_EVENT = {" >> $MAP_NAME
-	echo "   pos_x = $RIGHT_X" >> $MAP_NAME
+	echo "   pos_x = $LAST_COLUMN" >> $MAP_NAME
 	echo "   pos_y = 0" >> $MAP_NAME
 	echo "   script = \"set_sprite.lua\"" >> $MAP_NAME
 	echo "   param = ( \"$NUM\", \"\" )" >> $MAP_NAME
@@ -140,8 +178,8 @@ for NUM in `ls -d [0-9]* | sort -h`;do
 	let "CURRENT_EVENT=$CURRENT_EVENT+1"
 
 	echo "E$CURRENT_EVENT = {" >> $MAP_NAME
-	echo "   pos_x = $RIGHT_X" >> $MAP_NAME
-	echo "   pos_y = 2" >> $MAP_NAME
+	echo "   pos_x = $LAST_COLUMN" >> $MAP_NAME
+	echo "   pos_y = $LAST_LINE" >> $MAP_NAME
 	echo "   script = \"set_sprite.lua\"" >> $MAP_NAME
 	echo "   param = ( \"$NUM\", \"\" )" >> $MAP_NAME
 	echo "}" >> $MAP_NAME
