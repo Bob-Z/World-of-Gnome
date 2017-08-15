8296cb03ef2dcb168d513f7cf0a0ae68841ac87f
Bob-Z <carabobz@gmail.com>
2017-08-15 20:09:33 +0200

    [demo08] Fix map creation

--- data/demo08/script/add_map.lua
+++ data/demo08/script/add_map.lua
@@ -7,12 +7,12 @@ map_w = character_get_map_w(map)
 map_h = character_get_map_h(map)
 
 map_new(map_name,map_w,map_h,72,72)
-map_add_layer(map_name,0,map_w,map_h,72,72,"tile/gr1.png","fl")
+map_add_layer(map_name,0,-1,-1,-1,-1,"tile/gr1.png","fl")
 
 map_set_offscreen(map_name,"offscreen.lua")
 
-map_set_custom_column(map_name,0,0,54,-36)
-map_set_custom_column(map_name,0,1,54,36)
+map_set_custom_column(map_name,-1,0,54,-36)
+map_set_custom_column(map_name,-1,1,54,36)
 
 map_attribute_set(map_name,"x",map_x)
 map_attribute_set(map_name,"y",map_y)
