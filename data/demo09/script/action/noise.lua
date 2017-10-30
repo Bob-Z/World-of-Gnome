cab27926ee1f489947ae60e4e75ba7bac42c3f9a
Bob-Z <carabobz@gmail.com>
2017-10-30 21:50:04 +0100

    [demo09] Allow horses to make noise

---
+++ data/demo09/script/action/noise.lua
@@ -0,0 +1,7 @@
+function f (sound_file)
+
+player = player_get_id()
+
+character_effect(player,"effect/noise.lua",sound_file)
+
+end
---
+++ data/demo09/script/effect/noise.lua
@@ -0,0 +1,7 @@
+function f (sound_file)
+
+sound_play(sound_file)
+
+return 0
+
+end
