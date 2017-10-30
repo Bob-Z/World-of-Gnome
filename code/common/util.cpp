9549f42885da6b54e3d0d91e1f0f05c1a4bd7a36
Bob-Z <carabobz@gmail.com>
2017-10-30 16:16:44 +0100

    Fix crash when pointer is nullptr

--- code/common/util.cpp
+++ code/common/util.cpp
@@ -1,79 +1,87 @@
 /*
-   World of Gnome is a 2D multiplayer role playing game.
-   Copyright (C) 2013-2016 carabobz@gmail.com
+ World of Gnome is a 2D multiplayer role playing game.
+ Copyright (C) 2013-2016 carabobz@gmail.com
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 #include "common.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /***************************************************************************
-Return a string representing the checksum of the file or NULL on error
-filename is the directory + name
-The returned string MUST be FREED
-***************************************************************************/
+ Return a string representing the checksum of the file or nullptr on error
+ filename is the directory + name
+ The returned string MUST be FREED
+ ***************************************************************************/
 char * checksum_file(const char * filename)
 {
-	FILE *fp;
-	int ch;
+	FILE *fp = nullptr;
+	int ch = 0;
 	unsigned long long checksum = 5381;
 	char text[128];
 
-	fp = fopen(filename,"r"); // read mode
+	fp = fopen(filename, "r"); // read mode
 
-	if( fp == NULL ) {
-		return NULL;
+	if (fp == nullptr)
+	{
+		return nullptr;
 	}
 
-	while ( ( ch = fgetc(fp) ) != EOF ) {
+	while ((ch = fgetc(fp)) != EOF)
+	{
 		checksum = checksum * 33 ^ ch;
 	}
 
 	fclose(fp);
 
-	snprintf(text,128,"%llu",checksum);
+	snprintf(text, 128, "%llu", checksum);
 
-	wlog(LOGDEBUG,"Checksum for %s is %s",filename,text);
+	wlog(LOGDEBUG, "Checksum for %s is %s", filename, text);
 
 	return strdup(text);
 }
 
 /***************************************************************************
-the returned string MUST BE FREED
-***************************************************************************/
+ the returned string MUST BE FREED
+ ***************************************************************************/
 char * strconcat(const char * str, ...)
 {
 	va_list ap;
-	char * res = NULL;
+	char * res = nullptr;
 	int size = 0;
-	char * entry;
+	char * entry = nullptr;
+
+	if (str == nullptr)
+	{
+		return nullptr;
+	}
 
 	res = strdup(str);
 	size = strlen(res);
 
 	va_start(ap, str);
 
-	entry=va_arg(ap,char*);
-	while( entry != NULL ) {
+	entry = va_arg(ap, char*);
+	while (entry != nullptr)
+	{
 		size += strlen(entry);
-		res = (char *)realloc(res,size+1);
-		strcat(res,entry);
+		res = (char *) realloc(res, size + 1);
+		strcat(res, entry);
 
-		entry=va_arg(ap,char*);
+		entry = va_arg(ap, char*);
 	}
 
 	va_end(ap);
@@ -82,84 +90,97 @@ char * strconcat(const char * str, ...)
 }
 
 /*********************
-Free an array of element ( returned by entry_read_list, get_group_list ...)
-The last element of the list must be NULL
-*********************/
+ Free an array of element ( returned by entry_read_list, get_group_list ...)
+ The last element of the list must be nullptr
+ *********************/
 void deep_free(char ** to_delete)
 {
 	char ** current = to_delete;
 
-	if( to_delete == NULL ) {
+	if (to_delete == nullptr)
+	{
 		return;
 	}
 
-	while(*current) {
+	while (*current)
+	{
 		free(*current);
 		current++;
 	}
 
-	if(to_delete) {
+	if (to_delete)
+	{
 		free(to_delete);
 	}
 }
 
 /*********************
-Extract token from string
-Works just like strsep but delim is a full string, not separate characters
-*********************/
+ Extract token from string
+ Works just like strsep but delim is a full string, not separate characters
+ *********************/
 char * _strsep(char **stringp, const char *delim)
 {
 	char * next_delim;
 	char * start = *stringp;
 
-	if(*stringp == NULL) {
-		return NULL;
+	if (*stringp == nullptr)
+	{
+		return nullptr;
 	}
 
-	next_delim = strstr(*stringp,delim);
-	if(next_delim) {
+	next_delim = strstr(*stringp, delim);
+	if (next_delim)
+	{
 		*next_delim = 0;
 		*stringp = next_delim + strlen(delim);
-	} else {
-		*stringp = NULL;
+	}
+	else
+	{
+		*stringp = nullptr;
 	}
 	return start;
 }
 
 /********************************
-Add two arrays
-Return pointer must free (not deep_free)
-********************************/
+ Add two arrays
+ Return pointer must free (not deep_free)
+ ********************************/
 char ** add_array(char ** array1, char ** array2)
 {
 	int array_index = 0;
-	char ** current_array = NULL;
-	char ** ret_array = NULL;
+	char ** current_array = nullptr;
+	char ** ret_array = nullptr;
 
-	if( array1 != NULL ) {
+	if (array1 != nullptr)
+	{
 		current_array = array1;
-		while( *current_array != NULL ) {
+		while (*current_array != nullptr)
+		{
 			array_index++;
-			ret_array = (char **)realloc( ret_array, array_index * sizeof(char*) );
-			ret_array[array_index-1] = *current_array;
+			ret_array = (char **) realloc(ret_array,
+					array_index * sizeof(char*));
+			ret_array[array_index - 1] = *current_array;
 			current_array++;
 		}
 	}
 
-	if( array2 != NULL ) {
+	if (array2 != nullptr)
+	{
 		current_array = array2;
-		while( *current_array != NULL ) {
+		while (*current_array != nullptr)
+		{
 			array_index++;
-			ret_array = (char **)realloc( ret_array, array_index * sizeof(char*) );
-			ret_array[array_index-1] = *current_array;
+			ret_array = (char **) realloc(ret_array,
+					array_index * sizeof(char*));
+			ret_array[array_index - 1] = *current_array;
 			current_array++;
 		}
 	}
 
-	/* Terminal NULL */
+	/* Terminal nullptr */
 	array_index++;
-	ret_array = (char **)realloc( ret_array, array_index * sizeof(char*) );
-	ret_array[array_index-1] = NULL;
+	ret_array = (char **) realloc(ret_array, array_index * sizeof(char*));
+	ret_array[array_index - 1] = nullptr;
 
 	return ret_array;
 }
