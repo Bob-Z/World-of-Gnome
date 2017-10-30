4b2fdbe695529c5c1fa797151e62d469d0370d6e
Bob-Z <carabobz@gmail.com>
2017-10-30 16:45:06 +0100

    Cosmetic

--- code/client/parser_client.cpp
+++ code/client/parser_client.cpp
@@ -1,21 +1,21 @@
 /*
-   World of Gnome is a 2D multiplayer role playing game.
-   Copyright (C) 2013-2017 carabobz@gmail.com
+ World of Gnome is a 2D multiplayer role playing game.
+ Copyright (C) 2013-2017 carabobz@gmail.com
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 #include "common.h"
 #include "network_client.h"
@@ -29,63 +29,68 @@
 
 /***********************************
  Return RET_NOK on error
-***********************************/
-ret_code_t parse_incoming_data(context_t * context, Uint32 command, Uint32 command_size, char * data)
+ ***********************************/
+ret_code_t parse_incoming_data(context_t * context, Uint32 command,
+		Uint32 command_size, char * data)
 {
-	switch(command) {
-	case CMD_SEND_LOGIN_OK :
-		wlog(LOGDEBUG,"Received CMD_SEND_LOGIN_OK");
-		if(network_open_data_connection(context) == RET_NOK ) {
+	switch (command)
+	{
+	case CMD_SEND_LOGIN_OK:
+		wlog(LOGDEBUG, "Received CMD_SEND_LOGIN_OK");
+		if (network_open_data_connection(context) == RET_NOK)
+		{
 			return RET_NOK;
 		}
 		context_set_connected(context, true);
-		wlog(LOGUSER,"Successfully connected");
+		wlog(LOGUSER, "Successfully connected");
 		network_request_user_character_list(context);
-		wlog(LOGDEBUG,"Character list requested");
+		wlog(LOGDEBUG, "Character list requested");
 		break;
-	case CMD_SEND_LOGIN_NOK :
-		wlog(LOGDEBUG,"Received CMD_SEND_LOGIN_NOK");
+	case CMD_SEND_LOGIN_NOK:
+		wlog(LOGDEBUG, "Received CMD_SEND_LOGIN_NOK");
 		context_set_connected(context, false);
-		werr(LOGUSER,"Check your login and password (they are case sensitive)\n");
+		werr(LOGUSER,
+				"Check your login and password (they are case sensitive)\n");
 		exit(-1);
 		break;
-	case CMD_SEND_PLAYABLE_CHARACTER :
-		wlog(LOGDEBUG,"Received CMD_SEND_PLAYABLE_CHARACTER");
-		scr_create_add_playable_character(context,data);
+	case CMD_SEND_PLAYABLE_CHARACTER:
+		wlog(LOGDEBUG, "Received CMD_SEND_PLAYABLE_CHARACTER");
+		scr_create_add_playable_character(context, data);
 		screen_compose();
 		break;
-	case CMD_SEND_FILE :
-		wlog(LOGDEBUG,"Received CMD_SEND_FILE");
-		file_add(context,data,command_size);
+	case CMD_SEND_FILE:
+		wlog(LOGDEBUG, "Received CMD_SEND_FILE");
+		file_add(context, data, command_size);
 		screen_compose();
 		break;
-	case CMD_SEND_USER_CHARACTER :
-		wlog(LOGDEBUG,"Received CMD_SEND_USER_CHARACTER");
-		scr_select_add_user_character(context,data);
+	case CMD_SEND_USER_CHARACTER:
+		wlog(LOGDEBUG, "Received CMD_SEND_USER_CHARACTER");
+		scr_select_add_user_character(context, data);
 		screen_compose();
 		break;
-	case CMD_SEND_CONTEXT :
-		wlog(LOGDEBUG,"Received CMD_SEND_CONTEXT");
-		context_add_or_update_from_network_frame(context,data);
+	case CMD_SEND_CONTEXT:
+		wlog(LOGDEBUG, "Received CMD_SEND_CONTEXT");
+		context_add_or_update_from_network_frame(context, data);
 		screen_compose();
 		break;
-	case CMD_SEND_TEXT :
-		wlog(LOGDEBUG,"Received CMD_SEND_TEXT");
+	case CMD_SEND_TEXT:
+		wlog(LOGDEBUG, "Received CMD_SEND_TEXT");
 		textview_add_line(data);
 		break;
-	case CMD_SEND_ENTRY :
-		wlog(LOGDEBUG,"Received CMD_SEND_ENTRY");
-		if( entry_update(data) != -1 ) {
+	case CMD_SEND_ENTRY:
+		wlog(LOGDEBUG, "Received CMD_SEND_ENTRY");
+		if (entry_update(data) != -1)
+		{
 			screen_compose();
 		}
 		break;
-	case CMD_SEND_POPUP :
-		wlog(LOGDEBUG,"Received CMD_SEND_POPUP");
+	case CMD_SEND_POPUP:
+		wlog(LOGDEBUG, "Received CMD_SEND_POPUP");
 		ui_play_popup_add(data);
 		screen_compose();
 		break;
 	default:
-		werr(LOGDEV,"Unknown request from server");
+		werr(LOGDEV, "Unknown request from server");
 		return RET_NOK;
 		break;
 	}
--- code/server/parser_server.cpp
+++ code/server/parser_server.cpp
@@ -1,31 +1,32 @@
 /*
-   World of Gnome is a 2D multiplayer role playing game.
-   Copyright (C) 2013-2017 carabobz@gmail.com
+ World of Gnome is a 2D multiplayer role playing game.
+ Copyright (C) 2013-2017 carabobz@gmail.com
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
-#include "../common/common.h"
+#include "common.h"
 #include "character.h"
 #include "action.h"
 #include <string.h>
 
 /**************************************
-Return RET_NOK on error
-**************************************/
-ret_code_t parse_incoming_data(context_t * context, Uint32 command, Uint32 command_size, char * data)
+ Return RET_NOK on error
+ **************************************/
+ret_code_t parse_incoming_data(context_t * context, Uint32 command,
+		Uint32 command_size, char * data)
 {
 	char * value = nullptr;
 	char * fullname;
@@ -35,165 +36,199 @@ ret_code_t parse_incoming_data(context_t * context, Uint32 command, Uint32 comma
 	char * user_name;
 	char * password;
 
-	if( !context_get_connected(context) && ( command != CMD_REQ_LOGIN  && command != CMD_REQ_FILE) ) {
-		werr(LOGUSER,"Request from not authenticated client, close connection");
+	if (!context_get_connected(context)
+			&& (command != CMD_REQ_LOGIN && command != CMD_REQ_FILE))
+	{
+		werr(LOGUSER,
+				"Request from not authenticated client, close connection");
 		return RET_NOK;
 	}
 
-	switch(command) {
+	switch (command)
+	{
 	case CMD_REQ_LOGIN:
-		wlog(LOGDEBUG,"Received CMD_REQ_LOGIN");
-		user_name = _strsep(&data,NETWORK_DELIMITER);
-		password = _strsep(&data,NETWORK_DELIMITER);
+		wlog(LOGDEBUG, "Received CMD_REQ_LOGIN");
+		user_name = _strsep(&data, NETWORK_DELIMITER);
+		password = _strsep(&data, NETWORK_DELIMITER);
 
-		if(entry_read_string(PASSWD_TABLE, user_name, &value, PASSWD_KEY_PASSWORD,nullptr) == RET_NOK) {
+		if (entry_read_string(PASSWD_TABLE, user_name, &value,
+				PASSWD_KEY_PASSWORD, nullptr) == RET_NOK)
+		{
 			return RET_NOK;
 		}
-		if( strcmp(value, password) != 0) {
+		if (strcmp(value, password) != 0)
+		{
 			free(value);
-			werr(LOGUSER,"Wrong login for %s",user_name);
+			werr(LOGUSER, "Wrong login for %s", user_name);
 			// send answer
-			network_send_command(context, CMD_SEND_LOGIN_NOK, 0, nullptr, false);
+			network_send_command(context, CMD_SEND_LOGIN_NOK, 0, nullptr,
+					false);
 			// force client disconnection
 			return RET_NOK;
-		} else {
+		}
+		else
+		{
 			free(value);
 
-			if( context_set_username(context, user_name) == RET_NOK ) {
+			if (context_set_username(context, user_name) == RET_NOK)
+			{
 				return RET_NOK;
 			}
 			context_set_connected(context, true);
 
 			// send answer
 			network_send_command(context, CMD_SEND_LOGIN_OK, 0, nullptr, false);
-			wlog(LOGUSER,"Login successful for user %s",context->user_name);
+			wlog(LOGUSER, "Login successful for user %s", context->user_name);
 		}
 		break;
-	case CMD_REQ_PLAYABLE_CHARACTER_LIST :
-		wlog(LOGDEBUG,"Received CMD_REQ_PLAYABLE_CHARACTER_LIST");
+	case CMD_REQ_PLAYABLE_CHARACTER_LIST:
+		wlog(LOGDEBUG, "Received CMD_REQ_PLAYABLE_CHARACTER_LIST");
 		character_playable_send_list(context);
-		wlog(LOGDEBUG,"character list sent");
+		wlog(LOGDEBUG, "character list sent");
 		break;
-	case CMD_REQ_FILE :
+	case CMD_REQ_FILE:
 		i = 0;
-		elements[i] = _strsep(&data,NETWORK_DELIMITER);
-		while(elements[i]) {
+		elements[i] = _strsep(&data, NETWORK_DELIMITER);
+		while (elements[i])
+		{
 			i++;
-			elements[i] = _strsep(&data,NETWORK_DELIMITER);
+			elements[i] = _strsep(&data, NETWORK_DELIMITER);
 		}
 
-		if(elements[0]==nullptr || elements[1]==nullptr) {
-			werr(LOGDEV,"Received erroneous CMD_REQ_FILE");
+		if (elements[0] == nullptr || elements[1] == nullptr)
+		{
+			werr(LOGDEV, "Received erroneous CMD_REQ_FILE");
 			break;
 		}
-		wlog(LOGDEBUG,"Received CMD_REQ_FILE for %s",elements[0]);
+		wlog(LOGDEBUG, "Received CMD_REQ_FILE for %s", elements[0]);
 		// compare checksum
-		fullname = strconcat(base_directory,"/",elements[0],nullptr);
+		fullname = strconcat(base_directory, "/", elements[0], nullptr);
 
 		cksum = checksum_file(fullname);
 		free(fullname);
 
-		if( cksum == nullptr) {
-			werr(LOGUSER,"Required file %s doesn't exists",elements[0]);
+		if (cksum == nullptr)
+		{
+			werr(LOGUSER, "Required file %s doesn't exists", elements[0]);
 			break;
 		}
 
-		if( strcmp(elements[1],cksum) == 0 ) {
-			wlog(LOGDEBUG,"Client has already newest %s file",elements[0]);
+		if (strcmp(elements[1], cksum) == 0)
+		{
+			wlog(LOGDEBUG, "Client has already newest %s file", elements[0]);
 			free(cksum);
 			break;
 		}
 		free(cksum);
 
-		network_send_file(context,elements[0]);
-		wlog(LOGDEBUG,"File %s sent",elements[0]);
+		network_send_file(context, elements[0]);
+		wlog(LOGDEBUG, "File %s sent", elements[0]);
 		break;
-	case CMD_REQ_USER_CHARACTER_LIST :
-		wlog(LOGDEBUG,"Received CMD_REQ_USER_CHARACTER_LIST");
+	case CMD_REQ_USER_CHARACTER_LIST:
+		wlog(LOGDEBUG, "Received CMD_REQ_USER_CHARACTER_LIST");
 		character_user_send_list(context);
-		wlog(LOGDEBUG,"user %s's character list sent",context->user_name);
+		wlog(LOGDEBUG, "user %s's character list sent", context->user_name);
 		break;
-	case CMD_REQ_START :
-		if( context->in_game == false ) {
+	case CMD_REQ_START:
+		if (context->in_game == false)
+		{
 			context->id = strdup(data);
 			context->in_game = true;
 			context_update_from_file(context);
 			context_spread(context);
 			context_request_other_context(context);
 		}
-		wlog(LOGDEBUG,"Received CMD_REQ_START for %s /%s",context->user_name,context->id);
+		wlog(LOGDEBUG, "Received CMD_REQ_START for %s /%s", context->user_name,
+				context->id);
 		break;
-	case CMD_REQ_STOP :
-		wlog(LOGDEBUG,"Received CMD_REQ_STOP for %s /%s",context->user_name,context->id);
-		if( context->in_game == true ) {
+	case CMD_REQ_STOP:
+		wlog(LOGDEBUG, "Received CMD_REQ_STOP for %s /%s", context->user_name,
+				context->id);
+		if (context->in_game == true)
+		{
 			context->in_game = false;
-			if( context->map ) {
+			if (context->map)
+			{
 				free(context->map);
 			}
 			context->map = nullptr;
-			if( context->prev_map ) {
+			if (context->prev_map)
+			{
 				free(context->prev_map);
 			}
 			context->prev_map = nullptr;
-			if( context->id ) {
+			if (context->id)
+			{
 				free(context->id);
 			}
 			context->id = nullptr;
 			context_spread(context);
 		}
 		break;
-	case CMD_REQ_ACTION :
+	case CMD_REQ_ACTION:
 		i = 0;
 		elements[i] = nullptr;
-		elements[i] = _strsep(&data,NETWORK_DELIMITER);
-		while(elements[i]) {
-			i++;
-			elements[i] = _strsep(&data,NETWORK_DELIMITER);
-		}
-		elements[i+1] = nullptr;
-
-		wlog(LOGDEBUG,"Received CMD_REQ_ACTION %s from %s /%s",elements[0],context->user_name,context->character_name);
-
-		action_execute(context,elements[0],&elements[1]);
-		break;
-	case CMD_REQ_CREATE :
+		elements[i] = _strsep(&data, NETWORK_DELIMITER);
+		while (elements[i])
 		{
-			char * l_Id = _strsep(&data,NETWORK_DELIMITER);
-			char * l_Name = _strsep(&data,NETWORK_DELIMITER);
-			wlog(LOGDEBUG,"Received CMD_REQ_CREATE: ID=%s, NAME=%s",l_Id, l_Name);
-
-			char * l_FileName = nullptr;
-			l_FileName = file_new(CHARACTER_TABLE,l_Name);
-			if( l_FileName == nullptr ) {
-				werr(LOGUSER,"%s already exists",l_Name);
-				break;
-			}
-
-			if( file_copy(CHARACTER_TEMPLATE_TABLE,l_Id,CHARACTER_TABLE,l_Name) == false ) {
-				werr(LOGUSER,"Error copying character template %s to character %s (maybe template doesn't exists ?)",l_Id, l_Name);
-				file_delete(CHARACTER_TABLE, l_Name);
-				break;
-			}
-
-			if(entry_write_string(CHARACTER_TABLE, l_Name, l_Name, CHARACTER_KEY_NAME,nullptr) == RET_NOK) {
-				werr(LOGUSER,"Error setting character name %s", l_Name);
-				file_delete(CHARACTER_TABLE, l_Name);
-				break;
-			}
-
-			if( entry_add_to_list(USERS_TABLE,context->user_name,l_Name, USERS_CHARACTER_LIST, nullptr) == RET_NOK ) {	
-				werr(LOGUSER,"Error adding character %s to user %s", l_Name, context->user_name);
-				file_delete(CHARACTER_TABLE, l_Name);
-				break;
-			}
-
-			character_user_send(context, l_Name);
-
-			wlog(LOGDEBUG,"Successfully created: ID=%s, NAME=%s",l_Id, l_Name);
+			i++;
+			elements[i] = _strsep(&data, NETWORK_DELIMITER);
 		}
+		elements[i + 1] = nullptr;
+
+		wlog(LOGDEBUG, "Received CMD_REQ_ACTION %s from %s /%s", elements[0],
+				context->user_name, context->character_name);
+
+		action_execute(context, elements[0], &elements[1]);
+		break;
+	case CMD_REQ_CREATE:
+	{
+		char * l_Id = _strsep(&data, NETWORK_DELIMITER);
+		char * l_Name = _strsep(&data, NETWORK_DELIMITER);
+		wlog(LOGDEBUG, "Received CMD_REQ_CREATE: ID=%s, NAME=%s", l_Id, l_Name);
+
+		char * l_FileName = nullptr;
+		l_FileName = file_new(CHARACTER_TABLE, l_Name);
+		if (l_FileName == nullptr)
+		{
+			werr(LOGUSER, "%s already exists", l_Name);
+			break;
+		}
+
+		if (file_copy(CHARACTER_TEMPLATE_TABLE, l_Id, CHARACTER_TABLE, l_Name)
+				== false)
+		{
+			werr(LOGUSER,
+					"Error copying character template %s to character %s (maybe template doesn't exists ?)",
+					l_Id, l_Name);
+			file_delete(CHARACTER_TABLE, l_Name);
+			break;
+		}
+
+		if (entry_write_string(CHARACTER_TABLE, l_Name, l_Name,
+				CHARACTER_KEY_NAME, nullptr) == RET_NOK)
+		{
+			werr(LOGUSER, "Error setting character name %s", l_Name);
+			file_delete(CHARACTER_TABLE, l_Name);
+			break;
+		}
+
+		if (entry_add_to_list(USERS_TABLE, context->user_name, l_Name,
+				USERS_CHARACTER_LIST, nullptr) == RET_NOK)
+		{
+			werr(LOGUSER, "Error adding character %s to user %s", l_Name,
+					context->user_name);
+			file_delete(CHARACTER_TABLE, l_Name);
+			break;
+		}
+
+		character_user_send(context, l_Name);
+
+		wlog(LOGDEBUG, "Successfully created: ID=%s, NAME=%s", l_Id, l_Name);
+	}
 		break;
 	default:
-		werr(LOGDEV,"Unknown request %d from client",command);
+		werr(LOGDEV, "Unknown request %d from client", command);
 		return RET_NOK;
 	}
 
