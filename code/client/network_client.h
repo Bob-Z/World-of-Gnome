5f6193ddd568cac7b77ca82280598d38d2f606b3
Bob-Z <carabobz@gmail.com>
2017-11-01 16:06:00 +0100

    Cometic

--- code/client/network_client.h
+++ code/client/network_client.h
@@ -1,37 +1,39 @@
 /*
-   World of Gnome is a 2D multiplayer role playing game.
-   Copyright (C) 2013-2017 carabobz@gmail.com
+ World of Gnome is a 2D multiplayer role playing game.
+ Copyright (C) 2013-2017 carabobz@gmail.com
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 #ifndef NETWORK_CLIENT
 #define NETWORK_CLIENT
 
 #include "common.h"
 
-void network_login(context_t * context, const char * name, const char * password);
+void network_login(context_t * context, const char * name,
+		const char * password);
 void network_request_start(context_t * context, const char * id);
 void network_request_stop(context_t * context);
-void network_request_character_creation(context_t * context, const char * id, const char * name);
-void network_login(context_t * context, const char * name, const char * password);
+void network_request_character_creation(context_t * context, const char * id,
+		const char * name);
+void network_login(context_t * context, const char * name,
+		const char * password);
 void network_request_playable_character_list(context_t * context);
 void network_request_user_character_list(context_t * context);
-void network_send_action(context_t * context, const char * frame,...);
+void network_send_action(context_t * context, const char * frame, ...);
 ret_code_t network_connect(context_t * context, const char * hostname);
 ret_code_t network_open_data_connection(context_t * context);
 
 #endif
-
--- code/common/log.cpp
+++ code/common/log.cpp
@@ -1,21 +1,21 @@
 /*
-   World of Gnome is a 2D multiplayer role playing game.
-   Copyright (C) 2013-2016 carabobz@gmail.com
+ World of Gnome is a 2D multiplayer role playing game.
+ Copyright (C) 2013-2016 carabobz@gmail.com
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 #ifndef LOG_C
 #define LOG_C
@@ -28,50 +28,57 @@
 #include <SDL2/SDL.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 static int log_level = 0;
 
-const char *log_level_string[3] = {"user","dev","debug"};
+const char *log_level_string[3] =
+{ "user", "dev", "debug" };
 
-char ** file_filter = NULL;
+char ** file_filter = nullptr;
 int file_filter_num = 0;
 
-char ** func_filter = NULL;
+char ** func_filter = nullptr;
 int func_filter_num = 0;
 
 /**************************************************
-**************************************************/
+ **************************************************/
 void log_add_file_filter(const char * file)
 {
 	file_filter_num++;
-	file_filter = (char **)realloc(file_filter,sizeof(char*)*file_filter_num);
-	file_filter[file_filter_num-1] = strdup(file);
+	file_filter = (char **) realloc(file_filter,
+			sizeof(char*) * file_filter_num);
+	file_filter[file_filter_num - 1] = strdup(file);
 }
 
 /**************************************************
-**************************************************/
+ **************************************************/
 void log_add_func_filter(const char * func)
 {
 	func_filter_num++;
-	func_filter = (char **)realloc(func_filter,sizeof(char*)*func_filter_num);
-	func_filter[func_filter_num-1] = strdup(func);
+	func_filter = (char **) realloc(func_filter,
+			sizeof(char*) * func_filter_num);
+	func_filter[func_filter_num - 1] = strdup(func);
 }
 
 /**************************************************
-return 1 if filter is found
-**************************************************/
+ return 1 if filter is found
+ **************************************************/
 static int is_allowed_file(const char * file)
 {
 	int i;
 
-	if( file_filter == NULL ) {
+	if (file_filter == nullptr)
+	{
 		return 1;
 	}
 
-	for(i=0; i<file_filter_num; i++) {
-		if( !strcmp(file_filter[i],file) ) {
+	for (i = 0; i < file_filter_num; i++)
+	{
+		if (!strcmp(file_filter[i], file))
+		{
 			return 1;
 		}
 	}
@@ -80,18 +87,21 @@ static int is_allowed_file(const char * file)
 }
 
 /**************************************************
-return 1 if filter is found
-**************************************************/
+ return 1 if filter is found
+ **************************************************/
 static int is_allowed_func(const char * func)
 {
 	int i;
 
-	if( func_filter == NULL ) {
+	if (func_filter == nullptr)
+	{
 		return 1;
 	}
 
-	for(i=0; i<func_filter_num; i++) {
-		if( !strcmp(func_filter[i],func) ) {
+	for (i = 0; i < func_filter_num; i++)
+	{
+		if (!strcmp(func_filter[i], func))
+		{
 			return 1;
 		}
 	}
@@ -100,64 +110,80 @@ static int is_allowed_func(const char * func)
 }
 
 /**************************************************
-**************************************************/
-void log_print(int type,const char * file,const char * func,int line,FILE *stream,int level,const char * format, ...)
+ **************************************************/
+void log_print(int type, const char * file, const char * func, int line,
+		FILE *stream, int level, const char * format, ...)
 {
 	va_list ap;
 	char buf[10000];
 
-	if(level > log_level) {
+	if (level > log_level)
+	{
 		return;
 	}
 
-	if( !is_allowed_file(file) ) {
+	if (!is_allowed_file(file))
+	{
 		return;
 	}
 
-	if( !is_allowed_func(func) ) {
+	if (!is_allowed_func(func))
+	{
 		return;
 	}
 
-	va_start(ap,format);
-	vsnprintf(buf,sizeof(buf),format,ap);
+	va_start(ap, format);
+	vsnprintf(buf, sizeof(buf), format, ap);
 	va_end(ap);
 
-	if( type == TYPEERR ) {
-		fprintf(stream,"\e[31m");
+	if (type == TYPEERR)
+	{
+		fprintf(stream, "\e[31m");
 	}
 
-	if(log_level == LOGDEBUG) {
-		fprintf(stream,"%09d|%ld|%s(%d):%s|%s",SDL_GetTicks(),SDL_ThreadID(),file,line,func,buf);
-	} else if(log_level == LOGDEV) {
-		fprintf(stream,"%09d|%s",SDL_GetTicks(),buf);
-	} else if(log_level == LOGUSER) {
-		fprintf(stream,"%s",buf);
+	if (log_level == LOGDEBUG)
+	{
+		fprintf(stream, "%09d|%ld|%s(%d):%s|%s", SDL_GetTicks(), SDL_ThreadID(),
+				file, line, func, buf);
+	}
+	else if (log_level == LOGDEV)
+	{
+		fprintf(stream, "%09d|%s", SDL_GetTicks(), buf);
+	}
+	else if (log_level == LOGUSER)
+	{
+		fprintf(stream, "%s", buf);
 	}
 
-	if( type == TYPEERR ) {
-		fprintf(stream,"\e[0m");
+	if (type == TYPEERR)
+	{
+		fprintf(stream, "\e[0m");
 	}
 
-	fprintf(stream,"\n");
+	fprintf(stream, "\n");
 }
 
 /**************************************************
-**************************************************/
+ **************************************************/
 void log_set_level(char * log)
 {
-	if(log == NULL) {
+	if (log == nullptr)
+	{
 		return;
 	}
 
-	if(!strcmp(log,"0") || !strcasecmp(log,"user")) {
+	if (!strcmp(log, "0") || !strcasecmp(log, "user"))
+	{
 		log_level = LOGUSER;
 		return;
 	}
-	if(!strcmp(log,"1") || !strcasecmp(log,"dev")) {
+	if (!strcmp(log, "1") || !strcasecmp(log, "dev"))
+	{
 		log_level = LOGDEV;
 		return;
 	}
-	if(!strcmp(log,"2") || !strcasecmp(log,"debug")) {
+	if (!strcmp(log, "2") || !strcasecmp(log, "debug"))
+	{
 		log_level = LOGDEBUG;
 		return;
 	}
@@ -168,4 +194,3 @@ void log_set_level(char * log)
 #endif
 
 #endif // LOG_C
-
